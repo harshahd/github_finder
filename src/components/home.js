7c5867f6bc80c6d2261f7bbd4bab886e9f237a5f
Harsha HD <hdharsha4@gmail.com>
2022-08-23 10:17:46 +0530

    Initialized data fetching with axios

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,25 @@
 import React from "react";
+import axios from "axios";
 
 const Home=(props) => {
+    const [user,setUser]=React.useState(null);
+    React.useEffect(() => {
+axios.get("https://jsonplaceholder.typicode.com/posts").then((rsp) => {
+    setUser(rsp.data);
+}).catch((error) => {
+alert(error);    
+});
+    }, []);
+    if(!user)
+    return null;
+    // <!-- <p>{user.message} Read <a href={user.documentation_url}>Documentation</a> for more details.</p> -->
 return (
     <main>
     <h1>Github finder</h1>
     <p>View everybody's profiles, repositories and more with this application</p>
-             </main>
+    <h2>{user[3].title}</h2>
+    <p>{user[3].body}</p>
+                     </main>
 );
 };
 export default Home;
